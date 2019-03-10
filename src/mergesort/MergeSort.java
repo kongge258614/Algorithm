f5b019cf6dfbf45c22c3fb3d47cfa8b42370eb3b
liliang <dzliliang@126.com>
2019-03-10 21:28:58 +0800

    update

---
+++ src/mergesort/MergeSort.java
@@ -0,0 +1,10 @@
+package mergesort;
+
+/**
+ * @Author   liliang
+ * @Date     2019/3/10 21:05
+ * @Description: 归并排序
+ *
+ **/
+public class MergeSort {
+}
---
+++ src/quicksort/QuickSort.java
@@ -0,0 +1,12 @@
+package quicksort;
+
+/**
+ * @Author   liliang
+ * @Date     2019/3/10 21:07
+ * @Description :快速排序
+ **/
+public class QuickSort {
+    public static void main(String[] args) {
+        int [] arr ={56,33,71,21,13,6,27,38,99,87,126,76,99,13,37};
+    }
+}

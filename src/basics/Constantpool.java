f41cddc6255030c50eadabbe5e888dea1f2be319
liliang <dzliliang@126.com>
2019-04-29 14:00:06 +0800

    常量池问题

---
+++ src/basics/Constantpool.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class Constantpool {
+
+    /**
+     * 常量池
+     * @param args
+     */
+    public static void main(String[] args) {
+        Integer a =1,b = 1;
+
+        int c = 1,d = 1;
+
+        System.out.println(a== b);
+        System.out.println(a == c);
+        System.out.println( c == d);
+    }
+}
